9ab3e0a4bdf69c3cea46cafa111106391f3e3e28
Carlos Miguel <miguel.barbosa@academico.ifpb.edu.br>
2025-12-02 10:33:17 -0300

    adiciona a tela sobre

---
+++ my-app/.next/dev/server/chunks/ssr/[root-of-the-server]__3edb7492._.js
@@ -0,0 +1,171 @@
+module.exports = [
+"[project]/app/favicon.ico.mjs { IMAGE => \"[project]/app/favicon.ico (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript, Next.js Server Component)", ((__turbopack_context__) => {
+
+__turbopack_context__.n(__turbopack_context__.i("[project]/app/favicon.ico.mjs { IMAGE => \"[project]/app/favicon.ico (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)"));
+}),
+"[externals]/next/dist/shared/lib/no-fallback-error.external.js [external] (next/dist/shared/lib/no-fallback-error.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/shared/lib/no-fallback-error.external.js", () => require("next/dist/shared/lib/no-fallback-error.external.js"));
+
+module.exports = mod;
+}),
+"[project]/app/layout.js [app-rsc] (ecmascript, Next.js Server Component)", ((__turbopack_context__) => {
+
+__turbopack_context__.n(__turbopack_context__.i("[project]/app/layout.js [app-rsc] (ecmascript)"));
+}),
+"[project]/app/sobre/sobre.module.css [app-rsc] (css module)", ((__turbopack_context__) => {
+
+__turbopack_context__.v({
+  "container": "sobre-module__u5CazW__container",
+  "itemLista": "sobre-module__u5CazW__itemLista",
+  "lista": "sobre-module__u5CazW__lista",
+  "main": "sobre-module__u5CazW__main",
+  "subtitulo": "sobre-module__u5CazW__subtitulo",
+  "texto": "sobre-module__u5CazW__texto",
+  "titulo": "sobre-module__u5CazW__titulo",
+});
+}),
+"[project]/app/sobre/page.js [app-rsc] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>SobrePage
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__ = __turbopack_context__.i("[project]/app/sobre/sobre.module.css [app-rsc] (css module)");
+;
+;
+function SobrePage() {
+    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["Fragment"], {
+        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("main", {
+            className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].main,
+            children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("article", {
+                className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].container,
+                children: [
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].titulo,
+                        children: "Nossa História"
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 9,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h2", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].subtitulo,
+                        children: "A Biblioteca do Campus Monteiro"
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 11,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].texto,
+                        children: "A Biblioteca do Instituto Federal da Paraíba (IFPB) - Campus Monteiro é um órgão suplementar responsável por prover infraestrutura informacional para o suporte ao ensino, à pesquisa e à extensão. Localizada no coração do Cariri paraibano, nossa unidade busca democratizar o acesso ao conhecimento e incentivar o hábito da leitura em toda a comunidade acadêmica."
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 12,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].texto,
+                        children: "Nosso acervo conta com milhares de exemplares, incluindo livros didáticos, literatura técnica especializada, periódicos e bases de dados digitais, atendendo aos cursos técnicos e superiores oferecidos pela instituição."
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 20,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h2", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].subtitulo,
+                        children: "O Instituto Federal (IFPB)"
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 26,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].texto,
+                        children: "A trajetória do IFPB remonta a 1909, com a criação da Escola de Aprendizes Artífices da Paraíba. Ao longo de mais de um século, a instituição evoluiu passando pelas denominações de Escola Técnica Federal e CEFET, até se consolidar como Instituto Federal em 2008."
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 27,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].texto,
+                        children: "O Campus Monteiro representa a expansão dessa educação de excelência para o interior, transformando a realidade social e econômica da região através da formação qualificada de jovens e adultos."
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 33,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h2", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].subtitulo,
+                        children: "Estrutura e Serviços"
+                    }, void 0, false, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 39,
+                        columnNumber: 10
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("ul", {
+                        className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].lista,
+                        children: [
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].itemLista,
+                                children: "Empréstimo domiciliar e consulta local ao acervo físico."
+                            }, void 0, false, {
+                                fileName: "[project]/app/sobre/page.js",
+                                lineNumber: 41,
+                                columnNumber: 12
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].itemLista,
+                                children: "Acesso a computadores com internet para pesquisas acadêmicas."
+                            }, void 0, false, {
+                                fileName: "[project]/app/sobre/page.js",
+                                lineNumber: 44,
+                                columnNumber: 12
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].itemLista,
+                                children: "Salas de estudo em grupo e cabines individuais."
+                            }, void 0, false, {
+                                fileName: "[project]/app/sobre/page.js",
+                                lineNumber: 47,
+                                columnNumber: 12
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                className: __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$sobre$2f$sobre$2e$module$2e$css__$5b$app$2d$rsc$5d$__$28$css__module$29$__["default"].itemLista,
+                                children: "Orientação para normalização de trabalhos acadêmicos (ABNT)."
+                            }, void 0, false, {
+                                fileName: "[project]/app/sobre/page.js",
+                                lineNumber: 50,
+                                columnNumber: 12
+                            }, this)
+                        ]
+                    }, void 0, true, {
+                        fileName: "[project]/app/sobre/page.js",
+                        lineNumber: 40,
+                        columnNumber: 10
+                    }, this)
+                ]
+            }, void 0, true, {
+                fileName: "[project]/app/sobre/page.js",
+                lineNumber: 8,
+                columnNumber: 8
+            }, this)
+        }, void 0, false, {
+            fileName: "[project]/app/sobre/page.js",
+            lineNumber: 7,
+            columnNumber: 6
+        }, this)
+    }, void 0, false);
+}
+}),
+"[project]/app/sobre/page.js [app-rsc] (ecmascript, Next.js Server Component)", ((__turbopack_context__) => {
+
+__turbopack_context__.n(__turbopack_context__.i("[project]/app/sobre/page.js [app-rsc] (ecmascript)"));
+}),
+];
+
+//# sourceMappingURL=%5Broot-of-the-server%5D__3edb7492._.js.map
